7579aafad79802a07eaa1920ec6667d02b87ee54
deadwood <yang.jinghang@foxmail.com>
2020-03-12 00:18:22 +0800

    delete file after updating the file

--- update.py
+++ update.py
@@ -1,7 +1,9 @@
 import oss2
+import os
 from config import accessKey, accessSecret, endpoint
 
 
 def update(filename):
     bucket = oss2.Bucket(oss2.Auth(accessKey, accessSecret), endpoint, 'record-results')
     bucket.put_object_from_file(filename.split('/')[-1], filename)
+    os.remove(filename)
